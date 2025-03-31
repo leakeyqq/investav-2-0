c99d26c888abcb4a0bfccb69b03d075cfef78d43
leakeyqq <leakeymugendi3@gmail.com>
2025-03-31 21:28:58 +0300

    hi

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import Navbar from "@/components/navbar"
 
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Hui Finance',
+  description: 'Tokenized money market funds',
+  generator: 'LEE',
 }
 
 export default function RootLayout({
